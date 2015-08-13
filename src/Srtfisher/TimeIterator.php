4ce52ebcfcb94f31e532b4e8251df5fc07ed96dd
Sean Fisher <hi@seanfisher.co>
2015-08-13 01:23:04 -0400

    Up to code standards

--- src/Srtfisher/TimeIterator.php
+++ src/Srtfisher/TimeIterator.php
@@ -12,62 +12,63 @@ use Srtfisher\TimeIteratorException;
  * @author  Sean Fisher <hi@seanfisher.co>
  * @link    https://github.com/srtfisher/time-iterator
  */
-class TimeIterator implements \Iterator, \Countable {
+class TimeIterator implements \Iterator, \Countable
+{
   /**
    * Start Time of the Interval
    *
    * @type Carbon\Carbon
    */
-  protected $start;
+    protected $start;
 
   /**
    * End Time of the Current Interval
    *
    * @var Carbon\Carbon
    */
-  protected $end;
+    protected $end;
 
   /**
    * Interval Length in Seconds
    *
    * @var integer
    */
-  protected $intervalTime = 3600;
+    protected $intervalTime = 3600;
 
   /**
    * Current Time in the Interval
    *
    * @var Carbon\Carbon
    */
-  protected $currentTime;
+    protected $currentTime;
 
   /**
    * Callback to handle the results
    *
    * @var callable
    */
-  protected $callback;
+    protected $callback;
 
   /**
    * Results from the Iterator
    *
    * @var array
    */
-  protected $results = [];
+    protected $results = [];
 
   /**
    * Run Counter
    *
    * @var integer
    */
-  public $runCount = 0;
+    public $runCount = 0;
 
   /**
    * Current Key for Tracking in the Array Iterator
    *
    * @var interger
    */
-  protected $position = 0;
+    protected $position = 0;
 
   /**
    * Setup the Iterator to go though time
@@ -77,237 +78,237 @@ class TimeIterator implements \Iterator, \Countable {
    * @param integer Time in seconds between each interval
    * @param callable Callback function to retrieve the data for a time span
    */
-  public function __construct($start = NULL, $end = NULL, $intervalTime = NULL, $callback = NULL)
-  {
-    $this->setStart($start);
-    $this->setEnd($end);
-    $this->position = 0;
+    public function __construct($start = null, $end = null, $intervalTime = null, $callback = null)
+    {
+        $this->setStart($start);
+        $this->setEnd($end);
+        $this->position = 0;
 
-    if ($intervalTime) {
-      $this->intervalTime = intval($intervalTime);
-    }
+        if ($intervalTime) {
+            $this->intervalTime = intval($intervalTime);
+        }
 
-    if ($callback) {
-        if (! is_callable($callback)) {
-          throw new TimeIteratorException('Callback for `Timeiterator` is not callable.');
-        } else {
-          $this->callback = $callback;
+        if ($callback) {
+            if (! is_callable($callback)) {
+                throw new TimeIteratorException('Callback for `Timeiterator` is not callable.');
+            } else {
+                $this->callback = $callback;
+            }
         }
     }
-  }
 
   /**
    * Set the Start Time
    *
    * @param  integer|Guzzle Interger to subtract from now or a Guzzle Object
    */
-  public function setStart($time = 0)
-  {
-    $this->start = ($time instanceof Carbon) ? $time : Carbon::now()->subSeconds(intval($time));
-  }
+    public function setStart($time = 0)
+    {
+        $this->start = ($time instanceof Carbon) ? $time : Carbon::now()->subSeconds(intval($time));
+    }
 
   /**
    * Set the End Time
    *
    * @param  integer|Guzzle Interger to add from now or a Guzzle Object
    */
-  public function setEnd($time = 0)
-  {
-    $this->end = ($time instanceof Carbon) ? $time : Carbon::now()->addSeconds(intval($time));
-  }
+    public function setEnd($time = 0)
+    {
+        $this->end = ($time instanceof Carbon) ? $time : Carbon::now()->addSeconds(intval($time));
+    }
 
   /**
    * Retrieve the Interval Time
    *
    * @return  interval
    */
-  public function getInterval()
-  {
-    return $this->intervalTime;
-  }
+    public function getInterval()
+    {
+        return $this->intervalTime;
+    }
 
   /**
    * Retrieve the Start Time
    *
    * @return Guzzle
    */
-  public function getStart()
-  {
-    return $this->start;
-  }
+    public function getStart()
+    {
+        return $this->start;
+    }
 
   /**
    * Retrieve the End Time
    *
    * @return Guzzle
    */
-  public function getEnd()
-  {
-    return $this->end;
-  }
+    public function getEnd()
+    {
+        return $this->end;
+    }
 
   /**
    * Set the Interval
    *
    * @param  integer
    */
-  public function setInterval($interval)
-  {
-    $this->intervalTime = intval($interval);
-  }
+    public function setInterval($interval)
+    {
+        $this->intervalTime = intval($interval);
+    }
 
   /**
    * Set the Callback
    *
    * @param  callable
    */
-  public function setCallback($callback)
-  {
-    if (! is_callable($callback)) {
-      throw new TimeIteratorException('Callback for Timeiterator is not callable.');
-    }
+    public function setCallback($callback)
+    {
+        if (! is_callable($callback)) {
+            throw new TimeIteratorException('Callback for Timeiterator is not callable.');
+        }
 
-    $this->callback = $callback;
-  }
+        $this->callback = $callback;
+    }
 
   /**
    * Retrieve the Results
    *
    * @return  array
    */
-  public function getResults()
-  {
-    return $this->results;
-  }
+    public function getResults()
+    {
+        return $this->results;
+    }
 
   /**
    * Run the Iterator
    *
    * @return TimeIterator
    */
-  public function run()
-  {
-    // Sanity Check
-    if (! $this->callback) {
-      throw new TimeIteratorException('No callback passed to run iterator for.');
+    public function run()
+    {
+      // Sanity Check
+        if (! $this->callback) {
+            throw new TimeIteratorException('No callback passed to run iterator for.');
+        }
+
+        if ($this->start >= $this->end) {
+            throw new TimeIteratorException('Start time cannot be after or the same as the endtime.');
+        }
+
+      // Reset the Variables
+        $this->results = array();
+        $finished = false;
+        $this->currentTime = $this->start->copy();
+
+      // Begin the Loop
+        while (!$finished) {
+          // Ensuring that the callback always adds results
+            $resultCountBefore = count($this->results);
+
+          // Execute the callback
+          //
+          // Three arguments are sent:
+          //  1. Start time of the Interval
+          //  2. End time of the Interval
+          //  3. TimeIterator object to be used to set results for
+            call_user_func_array($this->callback, array(
+            $this->currentTime->copy(),
+            $this->currentTime->copy()->addSeconds($this->intervalTime),
+            $this
+            ));
+
+          // Ensure we don't miss anything
+            if (count($this->results) == $resultCountBefore) {
+                throw new TimeIteratorException('TimeInterval Error: no results added for interval');
+            }
+
+          // Bump to next time interval
+            $this->currentTime->addSeconds($this->intervalTime);
+
+          // See if we've run out of time
+            if ($this->currentTime >= $this->end) {
+                $finished = true;
+            }
+
+            $this->runCount++;
+        }
+
+        return $this;
     }
-
-    if ($this->start >= $this->end) {
-      throw new TimeIteratorException('Start time cannot be after or the same as the endtime.');
-    }
-
-    // Reset the Variables
-    $this->results = array();
-    $finished = false;
-    $this->currentTime = $this->start->copy();
-
-    // Begin the Loop
-    while (!$finished) {
-      // Ensuring that the callback always adds results
-      $resultCountBefore = count($this->results);
-
-      // Execute the callback
-      //
-      // Three arguments are sent:
-      //  1. Start time of the Interval
-      //  2. End time of the Interval
-      //  3. TimeIterator object to be used to set results for
-      call_user_func_array($this->callback, array(
-        $this->currentTime->copy(),
-        $this->currentTime->copy()->addSeconds($this->intervalTime),
-        $this
-      ));
-
-      // Ensure we don't miss anything
-      if (count($this->results) == $resultCountBefore) {
-        throw new TimeIteratorException('TimeInterval Error: no results added for interval');
-      }
-
-      // Bump to next time interval
-      $this->currentTime->addSeconds($this->intervalTime);
-
-      // See if we've run out of time
-      if ($this->currentTime >= $this->end) {
-        $finished = true;
-      }
-
-      $this->runCount++;
-    }
-
-    return $this;
-  }
 
   /**
    * Add Results to the Current Interval Set
    *
    * @param array
    */
-  public function addResults(array $data)
-  {
-    $this->results[] = array(
-      'data'          => $data,
-      'intervalStart' => $this->currentTime->copy(),
-      'intervalEnd'   => $this->currentTime->copy()->addSeconds($this->intervalTime)
-    );
-  }
+    public function addResults(array $data)
+    {
+        $this->results[] = array(
+        'data'          => $data,
+        'intervalStart' => $this->currentTime->copy(),
+        'intervalEnd'   => $this->currentTime->copy()->addSeconds($this->intervalTime)
+        );
+    }
 
   /**
    * Rewind the array pointer to the first element
    *
    * @return void
    */
-  public function rewind()
-  {
-    $this->position = 0;
-  }
+    public function rewind()
+    {
+        $this->position = 0;
+    }
 
   /**
    * Retrieve the Current Element
    *
    * @return array
    */
-  public function current()
-  {
-    return $this->results[$this->position];
-  }
+    public function current()
+    {
+        return $this->results[$this->position];
+    }
 
   /**
    * Retrieve the Current Key
    *
    * @return integer
    */
-  public function key()
-  {
-    return $this->position;
-  }
+    public function key()
+    {
+        return $this->position;
+    }
 
   /**
    * Moves the current position to the next element.
    *
    * @return void
    */
-  public function next()
-  {
-    $this->position++;
-  }
+    public function next()
+    {
+        $this->position++;
+    }
 
   /**
    * This method is called after Iterator::rewind() and Iterator::next() to check if the current position is valid.
    *
    * @return  boolean
    */
-  public function valid()
-  {
-    return isset($this->results[$this->position]);
-  }
+    public function valid()
+    {
+        return isset($this->results[$this->position]);
+    }
 
   /**
    * Retrieve the Current Count of the Result Set
    *
    * @return integer
    */
-  public function count()
-  {
-    return count($this->results);
-  }
+    public function count()
+    {
+        return count($this->results);
+    }
 }
